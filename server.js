2e0678299fe584c14723fac7da0a193cb8bd5be1
Nicolasjpsanchez <98576999+Nicolasjpsanchez@users.noreply.github.com>
2024-04-28 18:53:33 -0500

    hopefully fixed

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-// Import necessary libraries
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './backend/config/db.js';
 
-// Load environment variables
+// Initialize environment variables
 dotenv.config();
 
 const app = express();
 
+// Configure __dirname for ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // Middleware for parsing JSON bodies
 app.use(express.json());
-
-// Connect to MongoDB
-const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 // Serve static files from the 'frontend' directory
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Define a route for the root URL to ensure it serves the frontend if it exists
+// Connect to MongoDB
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
+
+// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html')); // Serve the static file directly
 });
 
-// Start the server function
+// Function to start the server
 const startServer = async () => {
     try {
         await connectDB(); // Ensure the database is connected before starting the server
